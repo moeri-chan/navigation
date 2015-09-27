0f2f252458ee751c20d7591784e26de7f79a47ef
moei <chan.moeri@gmail.com>
2015-09-27 21:11:57 +0000

    Got devise working correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Blog::Application.routes.draw do
-  get "log_in" => "sessions#new", :as => "log_in"
-  get "log_out" => "sessions#destroy", :as => "log_out"
+  devise_for :users
   get "posts/index"
 
   resources :posts do
