e87920577d4ca7173be064763ad8941c94c561ab
moei <chan.moeri@gmail.com>
2016-03-14 03:06:11 +0000

    updated so that microservice would work with reverse proxy url

--- spec/controllers/links_spec.rb
+++ spec/controllers/links_spec.rb
@@ -8,7 +8,7 @@ describe "links api tests", :type => :controller do
     it "should show a json object" do
       header "Content-Type", "application/json"
       header "Accept", "application/json"
-      request "/links"
+      request "navigation/links"
       expect(last_request.env["CONTENT_TYPE"]).to eq("application/json")
       expect(last_response.body).to include(@link.title)
     end
